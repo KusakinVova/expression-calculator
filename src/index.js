125a0737f44fcd534bfe45e73a957b91bd376fcc
Vladimir <kusakinvova@gmail.com>
2021-08-14 21:47:52 +0400

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,35 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    // ============================================
+    // удаляем пробелы
+    expr = expr.replace(/\s/g, '');
+    // ============================================
+    // проверка на правильность скобок
+    let open = 0;
+    for(let index = 0; index < expr.length; index++){
+        if( expr[index] === '(' ) open++;
+        if( expr[index] === ')' ) open--;
+        if(open<0) 
+            throw("ExpressionError: Brackets must be paired");
+    }
+    if(open !== 0) throw("ExpressionError: Brackets must be paired");
+    // ============================================
+    // проверка деления на ноль
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === "/") {
+            if (expr[i + 1] === "0") {
+                throw new Error('TypeError: Division by zero.');
+            }
+        }
+    };
+    // ============================================
+
+    const calculate = new Function(`return ${expr}`);
+    const result = calculate();
+    
+    return result;
+
 }
 
 module.exports = {
